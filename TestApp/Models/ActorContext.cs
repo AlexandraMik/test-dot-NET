9d3af1b747300fa594bfb1a377e0826ddda2ccc7
AlexandraMik <mik8856488@gmail.com>
2019-07-19 22:24:46 +0400

    create

---
+++ TestApp/Models/ActorContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TestApp.Models
+{
+    public class ActorContext:DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+
+        public DbSet<Buying> Buyings { get; set; }
+    }
+}
---
+++ TestApp/Models/Buying.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApp.Models
+{
+    public class Buying
+    {
+        public int BuyId { get; set; }
+
+        public string FIO { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Addres { get; set; }
+
+        public string Email { get; set; }
+    }
+}
